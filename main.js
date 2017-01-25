158d7f895617f4ae2697881c3494c552360abc9b
Travis Gorman <thefarbetween@me.com>
2017-01-24 21:40:03 -0600

    update readme

--- main.js
+++ main.js
@@ -8,17 +8,13 @@ window.addEventListener( 'keydown', (e) => {
 	pressed.splice(-code.length - 1, pressed.length - code.length)
 
 	if (pressed.join('').includes(code)) {
-		console.log( pressed.join('') + "!!!" )
 		win.push(pressed.join(''));
-		console.log( win )
-		if (win.length === 3) {
-			alert("You're a Winner!")
-		}
+		if (win.length === 3) {alert("You're a Winner!")}
+
 		cornify_add()
 	}
 	
 	console.log( pressed )
-	console.log( pressed.join('') )
 
 })
 
