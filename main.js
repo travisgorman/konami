e444e4d9bca2cdb6e6fc9c21170b72b52ce913d9
Travis Gorman <thefarbetween@me.com>
2017-01-24 20:57:40 -0600

    alerts when you type code correctly 3 times

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 const pressed = [];
 const code = 'travis';
+const win = [];
+
 window.addEventListener( 'keydown', (e) => {
 
 	pressed.push(e.key)
 	pressed.splice(-code.length - 1, pressed.length - code.length)
 
-	if (pressed.join('') === code){
+	if (pressed.join('').includes(code)) {
+		console.log( pressed.join('') + "!!!" )
+		win.push(pressed.join(''));
+		console.log( win )
+		if (win.length === 3) {
+			alert("You're a Winner!")
+		}
 		cornify_add()
 	}
-	console.log( code )
+	
 	console.log( pressed )
 	console.log( pressed.join('') )
 
