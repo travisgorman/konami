5d35870e89e0d52ffc0836f317bc51fe05b89476
Travis Gorman <thefarbetween@me.com>
2017-01-24 20:31:18 -0600

    joins array into string and looks for password

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ window.addEventListener( 'keydown', (e) => {
 	pressed.push(e.key)
 	pressed.splice(-code.length - 1, pressed.length - code.length)
 
+	if (pressed.join('') === code){
+		alert("DANG BRAH. that was siiiick")
+	}
+	console.log( code )
 	console.log( pressed )
+	console.log( pressed.join('') )
 
 })
 
