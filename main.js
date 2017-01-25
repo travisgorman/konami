9a503a849d04356b8d2a76d6dfa06a9e46c52aeb
Travis Gorman <thefarbetween@me.com>
2017-01-24 20:27:38 -0600

    logs array of pushed characters; only max length

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const code = 'travis';
 window.addEventListener( 'keydown', (e) => {
 
 	pressed.push(e.key)
+	pressed.splice(-code.length - 1, pressed.length - code.length)
+
+	console.log( pressed )
 
 })
 
